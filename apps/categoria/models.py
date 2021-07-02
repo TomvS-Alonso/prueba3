7508dea0c989e7e0b4676c2699538822cc7742b4
TomvS-Alonso <81627589+TomvS-Alonso@users.noreply.github.com>
2021-07-02 02:11:03 -0400

    Delete apps directory

--- apps/categoria/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Categoria(models.Model):
-    nombre_categoria = models.CharField('Nombre Categoria',max_length = 50)
-    descripcion_categoria = models.TextField('Descripcion Categoria', max_length = 150)
--- apps/principal/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import principal
-
-urlpatterns = [
-    path('',principal, name='principal')
-]
