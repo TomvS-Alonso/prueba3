3c0b890f23eb2d522f625dc85cb93918c08c8404
SebEspinoza <seba.espinozal@duocuc.cl>
2021-07-02 02:22:51 -0400

    Prueba

--- apps/carro/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from apps.principal.views import vistaCarro
-from .views import carrito, agregarCarrito, eliminarCarrito
-
-urlpatterns = [
-    path('', carrito, name='Carrito'),
-    path('carrito-agregar/<int:idProducto>', agregarCarrito, name='agregarCarro'),
-    path('carrito-eliminar/<int:idProducto>', eliminarCarrito, name='eliminarCarro')
-]
--- apps/personas/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import registro, iniciarSesion, salir
-
-urlpatterns = [
-    path('registro/', registro, name='Registro'),
-    path('salir/', salir, name='salir'),
-    path('iniciar-sesion/', iniciarSesion, name='iniciar_sesion')
-]
